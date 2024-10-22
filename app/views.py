497b6d6513de60e9f4e0a3903f05525fee886e0a
saunakpatel <saunak.4200@gmail.com>
2024-10-22 15:29:15 +0530

    save

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 print("hoiiiiiiiiiiiii")
 print("hello world")
-print("hello world1")
+
 def index(request):
     if "email" in request.session:
         uid = User.objects.get(email = request.session["email"])
